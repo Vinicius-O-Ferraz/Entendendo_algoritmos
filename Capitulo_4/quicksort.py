5dce8294c165edfa649cb9bdffb1e548eb0be4ad
Vinícius Ferreira Ferraz <146992032+Vinicius-O-Ferraz@users.noreply.github.com>
2025-01-06 14:07:09 -0300

    feat: implementar algoritmo quicksort recursivo
    - Implementar quicksort para ordenar uma lista de forma recursiva.
    - Dividir a lista em sublistas de elementos menores, iguais e maiores que o pivô.
    - Usar o primeiro elemento como pivô.
    - Adicionar exemplo de uso com uma lista desordenada.

--- Capitulo_4/quicksort.py
+++ Capitulo_4/quicksort.py
@@ -1,11 +1,20 @@
 def rec_sort(l):
+    # Caso base: Se a lista tiver menos de dois elementos, ela já está ordenada.
     if len(l) < 2:
         return l
     else:
+        # Escolhemos o primeiro elemento como pivô.
         pivo = l[0]
-        menores = [ i for i in l[1:] if i <= pivo]
-        maiores = [ i for i in l[1:]  if i > pivo]
+
+        # Criamos uma lista com os elementos menores ou iguais ao pivô.
+        menores = [i for i in l[1:] if i <= pivo]
+
+        # Criamos uma lista com os elementos maiores que o pivô.
+        maiores = [i for i in l[1:] if i > pivo]
+
+        # Ordenamos recursivamente as sublistas e as concatenamos com o pivô.
         return rec_sort(menores) + [pivo] + rec_sort(maiores)
-    
-l = [3,4,1,7,9,10,1,100]
-print(rec_sort(l))
+
+# Lista de exemplo
+l = [3, 4, 1, 7, 9, 10, 1, 100]
+print(rec_sort(l))  # Saída: [1, 1, 3, 4, 7, 9, 10, 100]
