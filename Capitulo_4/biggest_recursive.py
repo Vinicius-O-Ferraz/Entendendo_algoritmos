afb38c3240c0fc5f3ee52b390bf8caaf65a57262
Vinícius Ferreira Ferraz <146992032+Vinicius-O-Ferraz@users.noreply.github.com>
2025-01-06 14:04:29 -0300

    Update biggest_recursive.py
    fix: corrigir inicialização do maior valor na função recursiva

--- Capitulo_4/biggest_recursive.py
+++ Capitulo_4/biggest_recursive.py
@@ -1,16 +1,20 @@
-def biggest_rec(l:list, s = 0):
-    
-    if(len(l)== 1):
+def biggest_rec(l: list, s=0):
+    # Caso base: Se a lista tiver apenas um elemento, retornamos o maior número encontrado até agora.
+    if len(l) == 1:
         return s
     
     else:
+        # Comparação do segundo elemento da lista com o maior valor atual (s).
         if l[1] > s:
-            s =  l[1]
+            s = l[1]  # Atualizamos s com o novo maior valor encontrado.
 
-        return(biggest_rec(l[1:],s))
+        # Chamamos a função recursivamente, excluindo o primeiro elemento da lista.
+        return biggest_rec(l[1:], s)
 
-l = [1,2,3,4,5,6]
-print(biggest_rec(l))
+# Lista de exemplo 1
+l = [1, 2, 3, 4, 5, 6]
+print(biggest_rec(l))  # Saída: 6
 
-e = [3,4,1,7,9,10,1,100]
-print(biggest_rec(e))
+# Lista de exemplo 2
+e = [3, 4, 1, 7, 9, 10, 1, 100]
+print(biggest_rec(e))  # Saída: 100
