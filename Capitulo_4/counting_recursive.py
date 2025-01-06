005bd08d6e5a7bf5949029220d05956a3ee2aa22
Vinícius Ferreira Ferraz <146992032+Vinicius-O-Ferraz@users.noreply.github.com>
2025-01-06 14:05:55 -0300

    fix: corrigir função para somar os elementos da lista

--- Capitulo_4/counting_recursive.py
+++ Capitulo_4/counting_recursive.py
@@ -1,11 +1,15 @@
-def cout_sum(l:list, s = 0):
-    
-    if(len(l)== 0):
+def sum_elements(l: list, s=0):
+    # Caso base: Se a lista estiver vazia, retornamos a soma acumulada.
+    if len(l) == 0:
         return s
     
     else:
-        s = s + 1
-        return(cout_sum(l[1:],s))
+        # Adicionamos o primeiro elemento da lista ao acumulador (s).
+        s = s + l[0]
 
-l = [1,2,3,4,5,6]
-print(cout_sum(l))
+        # Chamamos a função recursivamente, excluindo o primeiro elemento da lista.
+        return sum_elements(l[1:], s)
+
+# Lista de exemplo
+l = [1, 2, 3, 4, 5, 6]
+print(sum_elements(l))  # Saída: 21
