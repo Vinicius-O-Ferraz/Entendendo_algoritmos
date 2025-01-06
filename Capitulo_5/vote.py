22c6c3c424e7bfa952ffeb544af4229e834cd61a
Vinícius Ferreira Ferraz <146992032+Vinicius-O-Ferraz@users.noreply.github.com>
2025-01-06 14:11:00 -0300

    feat: implementar sistema de verificação de votos usando hash table

--- Capitulo_5/vote.py
+++ Capitulo_5/vote.py
@@ -1,15 +1,19 @@
+# Dicionário que funciona como uma hash table para registrar os votos.
 voted = {}
 
 def vote_verification(name):
+    # Verifica se o nome já está na hash table.
     if voted.get(name):
-        print("Fuck off motherfucker!")
+        print("You cannot vote now, please leave immediately!")  # Mensagem caso o usuário já tenha votado.
         return
 
+    # Adiciona o nome à hash table, marcando que o usuário já votou.
     voted[name] = True
-    print("Vote now mate!")
+    print("Vote now mate!")  # Mensagem caso o usuário ainda não tenha votado.
     return
 
-vote_verification('paulinho')
-vote_verification('amélia')
-vote_verification('kid bengala')
-vote_verification('paulinho')
+# Testando a função com diferentes nomes.
+vote_verification('paulinho')  # Output: Vote now mate!
+vote_verification('amélia')    # Output: Vote now mate!
+vote_verification('kid bengala')  # Output: Vote now mate!
+vote_verification('paulinho')  # Output: You cannot vote now, please leave immediately
