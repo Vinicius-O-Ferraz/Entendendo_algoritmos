bedb18f1e86ba399ea39a4884badda72b1291f16
Vinícius Ferreira Ferraz <146992032+Vinicius-O-Ferraz@users.noreply.github.com>
2025-01-06 14:08:47 -0300

    feat: implementar soma recursiva de lista

--- Capitulo_4/sum_recursive.py
+++ Capitulo_4/sum_recursive.py
@@ -1,11 +1,13 @@
-def rec_sum(l:list, s = 0):
-    
-    if(len(l)== 0):
+def rec_sum(l: list, s=0):
+    # Caso base: Se a lista estiver vazia, retornamos a soma acumulada.
+    if len(l) == 0:
         return s
-    
     else:
+        # Adicionamos o primeiro elemento da lista ao acumulador (s).
         s = s + l[0]
-        return(rec_sum(l[1:],s))
+        # Chamamos a função recursivamente, excluindo o primeiro elemento da lista.
+        return rec_sum(l[1:], s)
 
-l = [i for i in range(1,5)]
-print(rec_sum(l))
+# Lista de exemplo gerada com números de 1 a 4.
+l = [i for i in range(1, 5)]  # Resultado: [1, 2, 3, 4]
+print(rec_sum(l))  # Saída: 10
